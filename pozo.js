2533c1dfa7655fa59865e46b7cb2d98654be0058
Luis Toledo <dti@alacoohperu.pe>
2023-02-06 13:05:37 -0500

    edit imag9

--- pozo.js
+++ pozo.js
@@ -20,13 +20,13 @@ async function init() {
     content2.style.display = "none";
     content3.style.display = "block";
     content4.style.display = "none";
-  }, 6000);
+  }, 5500);
   setTimeout(() => {
     content.style.display = "none";
     content2.style.display = "none";
     content3.style.display = "none";
     content4.style.display = "block";
-  }, 7000);
+  }, 500);
 }
 
 init();
