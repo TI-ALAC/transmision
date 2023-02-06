8481a2962511d5904a53c4363e34a935c61959ab
Luis Toledo <dti@alacoohperu.pe>
2023-02-06 16:46:33 -0500

    cambio en image6

--- pozo.js
+++ pozo.js
@@ -20,13 +20,13 @@ async function init() {
     content2.style.display = "none";
     content3.style.display = "block";
     content4.style.display = "none";
-  }, 6500);
+  }, 7000);
   setTimeout(() => {
     content.style.display = "none";
     content2.style.display = "none";
     content3.style.display = "none";
     content4.style.display = "block";
-  }, 7500);
+  }, 8000);
 }
 
 init();
