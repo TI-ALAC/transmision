321f20503883a80a71dc4e51fce2e1d5ca13c607
TatianaLlajaruna <llajaruna.tatiana@gmail.com>
2025-02-18 11:47:23 -0500

    probar transmision

--- pozo.js
+++ pozo.js
@@ -1,22 +1,9 @@
-var content = document.getElementById('contenido');
+
 var content2 = document.getElementById('contenido2');
-var content3 = document.getElementById('contenido3');
+
 async function init() {
-  content.style.display = "block";
-  setTimeout(() => {
-    content2.style.display = "none";
-    content3.style.display = "none";
-  }, 0);
-  setTimeout(() => {
-    content.style.display = "none";
-    content2.style.display = "block";
-    content3.style.display = "none";
-  }, 1000);
-  setTimeout(() => {
-    content.style.display = "none";
-    content2.style.display = "none";
-    content3.style.display = "block";
-  }, 5000);
+  content2.style.display = "block";
+  
 }
 
 init();
